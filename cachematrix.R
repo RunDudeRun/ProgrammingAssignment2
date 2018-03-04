82b545a6ee1b65b40be78b93ee4f39aeb6806d6b
Brett C <36033309+RunDudeRun@users.noreply.github.com>
2018-03-04 11:08:03 +1100

    complete stupid assignemnt

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
+## this function creates the matrix and sets global function variables
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+## this function computes the inverse of a matrix
+## if it already exists it retireves the cached version instead
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  mat <- x$get()    
+  m <- solve(mat, ...)
+  x$setInverse(m)
+  m
+  }
